eb34d1104654eda4e7cb3cc662b54e981f699b2b
Kristofer Berggren <kristofer.berggren@gmail.com>
2026-05-17 20:13:09 +0800

    fix cmake/python deprecations and bump ci actions/checkout to v5

--- make.sh
+++ make.sh
@@ -116,7 +116,11 @@ if [[ "${INSTALL}" == "1" ]]; then
   if [ "${OS}" == "Linux" ]; then
     cd build && sudo make install && cd .. || exiterr "install failed (linux), exiting."
   elif [ "${OS}" == "Darwin" ]; then
-    cd build && make install && cd .. || exiterr "install failed (mac), exiting."
+    if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+      cd build && sudo make install && cd .. || exiterr "install failed (mac), exiting."
+    else
+      cd build && make install && cd .. || exiterr "install failed (mac), exiting."
+    fi
   else
     exiterr "install failed (unsupported os ${OS}), exiting."
   fi
